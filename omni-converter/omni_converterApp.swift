e5e9516cb9edca92b2f6713694b400a79428f5b1
djordjejanjic <djordje.janjic@hotmail.com>
2026-03-24 10:27:50 +0100

    Initial Commit

---
+++ omni-converter/omni_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  omni_converterApp.swift
+//  omni-converter
+//
+//  Created by Djordje Janjic on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct omni_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
